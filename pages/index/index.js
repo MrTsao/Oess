0ffe76574d8ef6c5926a7cd8f90f6ee4a76bb891
MrTsao <rcode@163.com>
2017-09-01 19:17:34 +0800

    modify index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,56 @@ var app = getApp();
 Page({
   data: {
     PAGE: "INDEX",
-    userInfo: {}
+    userInfo: {},
+    background: [{
+      id: "item-1",
+      url: "https://www.yondo.cc/wxapp/images/102.png"
+    }, {
+      id: "item-2",
+      url: "https://www.yondo.cc/wxapp/images/101.png"
+    }],
+    delItems: [{
+      img: "../../image/index-img/5.png",
+      txt: "招考公告",
+      url: "real"
+    }, {
+      img: "../../image/index-img/7.png",
+      txt: "时事政治",
+      url: "real"
+    }],
+    showItems: [{
+      img: "../../image/index-img/1.png",
+      txt: "章节练习",
+      url: "real"
+    }, {
+      img: "../../image/index-img/2.png",
+      txt: "在线刷题",
+      url: "real"
+    }, {
+      img: "../../image/index-img/3.png",
+      txt: "历年真题",
+      url: "real"
+    }, {
+      img: "../../image/index-img/4.png",
+      txt: "模拟考试",
+      url: "real"
+    }, {
+      img: "../../image/index-img/6.png",
+      txt: "电子资料",
+      url: "real"
+    }, {
+      img: "../../image/index-img/8.png",
+      txt: "高频考题",
+      url: "real"
+    }, {
+      img: "../../image/index-img/9.png",
+      txt: "高频错题",
+      url: "real"
+    }, {
+      img: "../../image/index-img/10.png",
+      txt: "我的错题",
+      url: "real"
+    }]
   },
   //事件处理函数
   bindViewTap: function () {
